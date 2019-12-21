96006e097e60825bee13c19048558ab38e90b26d
fliconaNA <flicona@nativapps.co>
2019-12-20 23:25:44 -0500

    add more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { ShopComponent } from './components/shop/shop.component';
 import { VideosComponent } from './components/videos/videos.component';
 import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
 import { BodyComponent } from './components/body/body.component';
+import { PostComponent } from './components/post/post.component';
+import { VideoDetailComponent } from './components/videos/video-detail/video-detail.component';
+import { VideoComponent } from './components/videos/video/video.component';
+import { ItemComponent } from './components/shop/item/item.component';
+import { ItemDetailComponent } from './components/shop/item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +28,11 @@ import { BodyComponent } from './components/body/body.component';
     VideosComponent,
     NavBarComponent,
     BodyComponent,
+    PostComponent,
+    VideoDetailComponent,
+    VideoComponent,
+    ItemComponent,
+    ItemDetailComponent,
   ],
   imports: [
     BrowserModule,
